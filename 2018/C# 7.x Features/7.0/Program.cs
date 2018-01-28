306b8115d033fd8be7d281f2735a5d001ae97199
Nicolai Stoy <nicolai.stoy@gmail.com>
2018-01-28 15:05:27 +0100

    Added Example for new out variable

--- 2018/C# 7.x Features/7.0/Program.cs
+++ 2018/C# 7.x Features/7.0/Program.cs
@@ -11,7 +11,37 @@ namespace _7._0
         static void Main(string[] args)
         {
 
+            #region OutVariable1
+            int result;
+            string answer = "42";
 
+            if (int.TryParse(answer, out result))
+                Console.WriteLine($"The answer to the ultimate question of life, the universe and everything is {result}!");
+            else
+                Console.WriteLine("Nope");
+            #endregion
+
+
+            #region OutVariable2
+            string answer2 = "42";
+
+            //Console.WriteLine(result2);
+
+            if (int.TryParse(answer2, out var result2))
+                Console.WriteLine($"The answer to the ultimate question of life, the universe and everything is {result2}!");
+            else
+                Console.WriteLine("Nope");
+
+
+            Console.WriteLine($"Result: {result2}");
+
+            #endregion
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
     }
+
+
+
 }
